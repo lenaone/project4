164f08c0062e41c8899f8cbe6907bc8866f349ea
Lena <sulyh9@gmail.com>
2019-03-21 22:12:57 +1100

    fix js not working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,10 +2,13 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-  var displayDropdownMenu = function() {
-    dropdown_menus.classList.toggle('hidden')
+  window.onload = function(e) {
+    var displayDropdownMenu = function() {
+      dropdown_menus.classList.toggle('hidden')
+    }
+  
+    var icon =  document.querySelector('.user-avatar')
+    var dropdown_menus = document.querySelector('.dropdown-div')
+    icon.addEventListener('click', displayDropdownMenu)   
   }
-
-  var icon =  document.querySelector('.user-avatar')
-  var dropdown_menus = document.querySelector('.dropdown-div')
-  icon.addEventListener('click', displayDropdownMenu)   
+  
